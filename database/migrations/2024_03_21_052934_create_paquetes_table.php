ee66406d737cf03f677785b981ecc1ad13ac6c22
Erik Romero <romeroerik04@gmail.com>
2024-03-21 22:31:08 -0700

    Primer commit Api Laravel

---
+++ database/migrations/2024_03_21_052934_create_paquetes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('paquetes', function (Blueprint $table) {
+            $table->id();
+            $table->integer('cliente_id');
+            $table->string('descripcion');
+            $table->double('peso');
+            $table->string('contenido');
+            $table->string('estado');
+            $table->date('fecha_envio');
+            $table->date('fecha_entrega')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('paquetes');
+    }
+};
---
+++ database/migrations/2024_03_21_053732_create_envios_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('envios', function (Blueprint $table) {
+            $table->id();
+            $table->integer('paquete_id');
+            $table->integer('conductor_id');
+            $table->string('direccion_origen');
+            $table->string('direccion_destino');
+            $table->date('fecha_programada');
+            $table->double('costo');
+            $table->string('estado');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('envios');
+    }
+};
